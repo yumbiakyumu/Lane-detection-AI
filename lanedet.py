d1606138edd0ffc0285efc5f1c216248e7881009
Boniface Kyumu <yumbiakyumu@gmail.com>
2023-09-12 15:10:35 +0300

    Bug Fixes

--- lanedet.py
+++ lanedet.py
@@ -3,10 +3,12 @@ import matplotlib.pylab as apl
 import cv2
 import numpy as ny
 
+
+
 # Read an image named 'lane9.jpg' using OpenCV
 image = cv2.imread('lane9.jpg')
 # Convert the image from RGB to BGR color space
-image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 # Print the shape (dimensions) of the image
 print(image.shape)
@@ -20,24 +22,27 @@ important_region_vertices = [
     (width/1.95, height/1.55),
     (width, height)
 ]
-
 # Define a function to extract the important region from an image
 def important_region(img, vertices):
     # Create a mask of zeros with the same dimensions as the image
     mask = ny.zeros_like(img)
     # Get the number of color channels in the image
-    channel_count = img.shape[2]
+    #channel_count = img.shape[2]
     # Create a color value to match for filling the mask (white color)
-    match_mask_color = (255,) * channel_count
+    match_mask_color = 255
     # Fill the polygon defined by 'vertices' with the match_mask_color
     cv2.fillPoly(mask, vertices, match_mask_color)
     # Perform a bitwise AND operation between the image and the mask
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
+grayscale_image=cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+canny_image=cv2.Canny(grayscale_image, 100, 200)
 # Get the masked image using the 'important_region' function
-masked_image = important_region(image, ny.array([important_region_vertices], ny.int32))
+masked_image = important_region(canny_image, ny.array([important_region_vertices], ny.int32))
+
 
 # Display the masked image using Matplotlib
 apl.imshow(masked_image)
 apl.show()
+
